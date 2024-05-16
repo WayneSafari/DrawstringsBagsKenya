64d05b0a5d42f07e6955ecb48a5324268a0fad94
AmonKogei <amonlatimo101@gmail.com>
2024-05-16 18:55:57 +0300

    All Rights Reserved

---
+++ assets/scripts/Chatbot.js
@@ -0,0 +1,30 @@
+// Hide the chat container on page load
+document.getElementById("chat-container").style.display = "none";
+document.getElementById("user-input").addEventListener("keydown", function(event) {
+  if (event.keyCode === 13) { // Check if Enter key is pressed
+    event.preventDefault(); // Prevent default behavior (e.g., new line in textarea)
+    sendMessage(); // Call sendMessage function
+  }
+});
+document.querySelector('.chat-toggle').addEventListener('click', function() {
+  document.getElementById('user-input').focus();
+});
+
+function sendMessage() {
+  var userInput = document.getElementById("user-input").value;
+  if (userInput.trim() !== "") {
+    var chatBox = document.getElementById("chat-box");
+    chatBox.innerHTML += "<div class='chat-message user-message'><div class='message-text'><p>" + userInput + "</p></div></div>";
+    // Simulate bot response
+    setTimeout(function() {
+      chatBox.innerHTML += "<div class='chat-message bot-message'><img src='assets/images/drawstringschat.png' alt='Chatbot Image' class='chat-message-image'><div class='message-text'><p>Text us on Whatsapp via +25476726878989889</p></div></div>";
+      chatBox.scrollTop = chatBox.scrollHeight;
+    }, 500);
+    document.getElementById("user-input").value = "";
+  }
+}
+
+function toggleChat() {
+  var chatContainer = document.getElementById("chat-container");
+  chatContainer.style.display = chatContainer.style.display === "none" ? "block" : "none";
+}
---
+++ assets/scripts/main.js
@@ -0,0 +1,15 @@
+function showPage(pageName) {
+    // Hide all pages
+    var pages = document.querySelectorAll('.page');
+    pages.forEach(function(page) {
+      page.style.display = 'none';
+    });
+  
+    // Show the selected page
+    var selectedPage = document.getElementById(pageName);
+    if (selectedPage) {
+      selectedPage.style.display = 'block';
+    } else {
+      console.error('Page not found:', pageName);
+    }
+  }
